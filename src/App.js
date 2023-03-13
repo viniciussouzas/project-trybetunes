31b15e8297c3ee11998df8854898ca5ac444c4fc
Vinícius Souza <tradutorvinicius@gmail.com>
2023-03-13 17:46:34 -0300

    Resolve requisito 2

--- src/App.js
+++ src/App.js
@@ -7,14 +7,65 @@ import NotFound from './pages/NotFound';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import Search from './pages/Search';
+import { createUser } from './services/userAPI';
+
+const MIN_LENGTH = 3;
 
 class App extends React.Component {
+  state = {
+    login: '',
+    loading: false,
+    redirect: false,
+  };
+
+  onInputChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState(({
+      [name]: value,
+    }));
+  };
+
+  handleCreateUser = async () => {
+    const { login } = this.state;
+
+    this.setState({
+      loading: true,
+    });
+
+    const response = await createUser({ name: login });
+
+    if (response === 'OK') {
+      this.setState({
+        loading: false,
+        redirect: true,
+      });
+    }
+  };
+
   render() {
+    const { login, loading, redirect } = this.state;
+
+    const buttonDisabled = login.length < MIN_LENGTH === true;
+
     return (
       <div>
         <p>TrybeTunes</p>
         <Switch>
-          <Route exact path="/" component={ Login } />
+          <Route
+            exact
+            path="/"
+            render={ (props) => (<Login
+              { ...props }
+              onInputChange={ this.onInputChange }
+              handleCreateUser={ this.handleCreateUser }
+              login={ login }
+              loading={ loading }
+              redirect={ redirect }
+              isButtonDisabled={ buttonDisabled }
+            />) }
+          />
           <Route exact path="/search" component={ Search } />
           <Route exact path="/album/:id" component={ Album } />
           <Route exact path="/favorites" component={ Favorites } />
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,48 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
+import Loading from '../Loading';
 
 class Login extends Component {
   render() {
+    const { login, loading, redirect,
+      isButtonDisabled, onInputChange, handleCreateUser } = this.props;
+
+    if (loading) return <Loading />;
+
+    if (redirect) return <Redirect to="/search" />;
+
     return (
       <div data-testid="page-login">
-
+        <form>
+          <input
+            type="text"
+            name="login"
+            value={ login }
+            onChange={ onInputChange }
+            data-testid="login-name-input"
+            placeholder="Insira seu Nome"
+          />
+          <button
+            data-testid="login-submit-button"
+            disabled={ isButtonDisabled }
+            onClick={ handleCreateUser }
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  login: PropTypes.string.isRequired,
+  loading: PropTypes.bool.isRequired,
+  redirect: PropTypes.bool.isRequired,
+  isButtonDisabled: PropTypes.bool.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  handleCreateUser: PropTypes.func.isRequired,
+};
 
 export default Login;
